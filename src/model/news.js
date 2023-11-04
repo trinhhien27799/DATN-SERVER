2017adc229355d469c1c40c0568c12a83c86040d
Minh Tô <milkissily@gmail.com>
2023-11-05 02:03:16 +0700

    rollback

--- src/model/news.js
+++ src/model/news.js
@@ -3,15 +3,10 @@ const Schema = mongoose.Schema
 
 
 
-const News = new Schema({
+const Banner = new Schema({
     image: { type: String, require: true },
-    brand: { type: String },
-    content: [{
-        title: { type: String },
-        content: { type: String },
-        image: { type: String },
-        productId: { type: String },
-    }],
+    title: { type: String, require: true },
+    keyword: { type: String },
     time: { type: Date, default: Date.now }
 }, {
     collection: "Banner"
@@ -19,4 +14,4 @@ const News = new Schema({
 
 
 
-module.exports = mongoose.model('News', News) 
+module.exports = mongoose.model('Banner', Banner) 

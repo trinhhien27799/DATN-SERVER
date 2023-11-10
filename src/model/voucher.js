c7407f890ed22bca62cc4d185e7b70211805387c
Minh Tô <milkissily@gmail.com>
2023-11-10 23:35:53 +0700

    cập nhật

--- src/model/voucher.js
+++ src/model/voucher.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 
 const Voucher = new Schema({
     username: { type: String },
-    code: { type: String, require: true, unique: true },
+    code: { type: String, require: true},
     description: { type: String, require: true },
     release_date: { type: Date, default: Date.now },
     condition: { type: Number, default: 0, require: true },// Điều kiện sử dụng, giá trị hóa đơn bao gồm phí vận chuyển

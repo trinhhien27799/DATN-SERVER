3bba99c389249df3a53d7314fbb7273a0b1f42d1
Minh Tô <milkissily@gmail.com>
2023-11-22 23:43:18 +0700

    up

--- src/controller/web/bill.controller.js
+++ src/controller/web/bill.controller.js
@@ -6,10 +6,10 @@ const Variations = require("../../model/variations");
 class Controller {
   async list(req, res) {
     try {
-      const array = await Bill.find({status: req.query.status});
+      const array = await Bill.find({ status: req.query.status });
 
-      const amount = await Bill.find({status: 0});
-      const amount2 = await Bill.find({status: 1});
+      const amount = await Bill.find({ status: 0 });
+      const amount2 = await Bill.find({ status: 1 });
 
       for (let i = 0; i < array.length; i++) {
 
@@ -29,7 +29,6 @@ class Controller {
           }
         }
       }
-      console.log(array);
       res.render("bill/viewBill", { layout: "layouts/main", data: array, amount, amount2, req });
     } catch (error) {
       res.json(error);
@@ -51,27 +50,22 @@ class Controller {
       const bill = await Bill.findById(id);
       if (bill && bill.status == 0) {
         bill.status = 1;
-      }else if (bill && bill.status == 1) {
+      } else if (bill && bill.status == 1) {
         bill.status = 2;
       }
       await bill.save();
       res.redirect(`/bill/?status=${bill.status - 1}`);
+      if (bill.status != 2) return
       await Promise.all(bill.products.map(async (item) => {
         const cacheCheck = await Cache.findOne({ username: bill.username, varitationId: item.variations_id })
-        // if (cacheCheck) {
-        //   await cacheCheck.deleteOne()
-        //   await Cache.create(cacheCheck)
-        // } else {
-        //   const variations = await Variations.findById(item.variations_id)
-        //   await Cache.create({ username: bill.username, productId: variations.productId, varitationId: item.variations_id })
-        // }
-
-        if (!cacheCheck) {
+        if (cacheCheck) {
+          await cacheCheck.deleteOne()
+          await Cache.create({ username: cacheCheck.username, productId: cacheCheck.productId, varitationId: cacheCheck.varitationId })
+        } else {
           const variations = await Variations.findById(item.variations_id)
           await Cache.create({ username: bill.username, productId: variations.productId, varitationId: item.variations_id })
         }
       }))
-
     } catch (error) {
       console.log(error);
       res.json(error);
